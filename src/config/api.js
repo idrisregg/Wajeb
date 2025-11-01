c88a4fa197bcaeb974dea88e5f2dc9a8ef913358
idrisregg <idrisregg@gmail.com>
2025-11-01 11:37:01 +0100

    Ui stuff

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 const config = {
-    API_BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3004'
+    API_BASE_URL: import.meta.env.VITE_API_BASE_URL
 };
 
 export const currentConfig = config;
